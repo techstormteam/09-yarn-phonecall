cab6132010d34f8a264e3ee7a0991286518404f8
vinh.thien0301 <vinh.thien0301@gmail.com>
2014-10-17 15:28:16 +0700

    fix

---
+++ platforms/android/ant-build/HelloWorld-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/macbookair/09-yarn-phonecall/platforms/android/ant-build/HelloWorld-debug-unaligned.apk : \
+/Users/macbookair/09-yarn-phonecall/platforms/android/ant-build/HelloWorld.ap_ \
+/Users/macbookair/09-yarn-phonecall/platforms/android/ant-build/classes.dex \
+/Users/macbookair/09-yarn-phonecall/platforms/android/CordovaLib/ant-build/classes.jar \
