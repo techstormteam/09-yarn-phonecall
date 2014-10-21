e39ef294357069c84e4f8ebd79fc6b77d66437e0
vinhthien <vinh.thien0301@gmail.com>
2014-10-21 23:08:22 +0700

    dial page

--- platforms/android/assets/www/js/dial.js
+++ platforms/android/assets/www/js/dial.js
@@ -12,6 +12,8 @@ var footer2nd = $('[data-id="footer-2nd"]');
 var footer2ndDiv = $('[data-id="footer-2nd"] div');
 var remain;
 
+alert('dial05');
+
 function divide(element, numerator, denominator) {
     return (element * numerator) / denominator;
 }
@@ -25,25 +27,27 @@ function updateSize() {
 
     //AVATAR
 //    $('[data-id="main"]').css('height', (totalHeight / 6) * 3);
-    main.css('height', Math.ceil((totalHeight / 6) * 3));
-    mainImg.css('height', Math.ceil((totalHeight / 6) * 3));
+    main.css('height', (totalHeight / 6) * 3);
+    mainImg.css('height', (totalHeight / 6) * 3);
+//    main.css('height', Math.ceil((totalHeight / 6) * 3));
+//    mainImg.css('height', Math.ceil((totalHeight / 6) * 3));
 //    $('[data-id="avatar"] img').css('height', (totalHeight / 6) * 3);
 
     //SUBFOOTER
-    subFoot.css('height', Math.ceil(totalHeight / 6));
-    timer.css('line-height', Math.ceil(totalHeight / 12) + 'px');
-    endCall.css('height', Math.ceil(totalHeight / 12));
+    subFoot.css('height',(totalHeight / 6));
+    timer.css('line-height', (totalHeight / 12) + 'px');
+    endCall.css('height', (totalHeight / 12));
 
     //FOOTER
-    footer.css('height', Math.ceil(totalHeight / 6));
+    footer.css('height', (totalHeight / 6) + 2);
 
     //FOOTER-1ST
-    footer1st.css('height', Math.ceil(totalHeight / 12));
-    footer1stDiv.css('height', Math.ceil(totalHeight / 12));
+    footer1st.css('height', (totalHeight / 12));
+    footer1stDiv.css('height', (totalHeight / 12));
 
     //FOOTER-2ND
-    footer2nd.css('height', Math.ceil(totalHeight / 12));
-    footer2ndDiv.css('height', Math.ceil(totalHeight / 12));
+    footer2nd.css('height', (totalHeight / 12));
+    footer2ndDiv.css('height', (totalHeight / 12));
     
 //    alert(parseInt(header.css('height')) + parseInt(main.css('height')) + parseInt(subFoot.css('height')) + parseInt(footer.css('height')));
 //    remain += parseInt(header.css('height')) + parseInt(main.css('height')) + parseInt(subFoot.css('height')) + parseInt(footer.css('height')); 
