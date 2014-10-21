726c72ac4898a14b1de3e45b5cf799601426a0a3
vinh.thien0301 <vinh.thien0301@gmail.com>
2014-10-21 19:53:45 +0700

    add enable all video and audio codecs

--- platforms/android/src/com/techstorm/yarn/Yarn.java
+++ platforms/android/src/com/techstorm/yarn/Yarn.java
@@ -22,15 +22,16 @@ package com.techstorm.yarn;
 import static android.content.Intent.ACTION_MAIN;
 
 import org.apache.cordova.CordovaActivity;
+import org.linphone.LinphoneManager;
 import org.linphone.LinphoneService;
+import org.linphone.core.LinphoneCore;
+import org.linphone.core.LinphoneCoreException;
+import org.linphone.core.PayloadType;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.telephony.PhoneStateListener;
-import android.telephony.TelephonyManager;
-import android.util.Log;
 
 public class Yarn extends CordovaActivity
 {
@@ -86,8 +87,27 @@ public class Yarn extends CordovaActivity
 		mHandler.postDelayed(new Runnable() {
 			@Override
 			public void run() {
-				// Nothing to do
+//				try {
+//					enableAllAudioCodecs();
+//					enableAllVideoCodecs();
+//				} catch (LinphoneCoreException e) {
+//					e.printStackTrace();
+//				}
 			}
 		}, 1000);
+	}
+    
+    private void enableAllAudioCodecs() throws LinphoneCoreException {
+		LinphoneCore lc = LinphoneManager.getLcIfManagerNotDestroyedOrNull();
+		for (final PayloadType pt : lc.getAudioCodecs()) {
+			LinphoneManager.getLcIfManagerNotDestroyedOrNull().enablePayloadType(pt, true);
+		}
+	}
+    
+    private void enableAllVideoCodecs() throws LinphoneCoreException {
+		LinphoneCore lc = LinphoneManager.getLcIfManagerNotDestroyedOrNull();
+		for (final PayloadType pt : lc.getVideoCodecs()) {
+			LinphoneManager.getLcIfManagerNotDestroyedOrNull().enablePayloadType(pt, true);
+		}
 	}
 }
