beb73b01de666dc04a473aafc7e1bbd8d50a526e
Viet Anh <vietanh.sgu@gmail.com>
2014-10-23 15:45:25 +0700

    signup fixed

--- platforms/android/assets/www/js/signup-old.js
+++ platforms/android/assets/www/js/signup-old.js
@@ -2,45 +2,37 @@ var totalHeight;
 var totalWidth;
 var oneTenth;
 var header = $('[data-id="header"]');
-var video = $('video');
+
+//alert('signup');
 
 function updateSize() {
     totalHeight = $(window).height();
     totalWidth = $(window).width();
     oneTenth = totalHeight / 10;
     oneSixth = totalHeight / 6;
-    oneNineth = totalHeight / 9;
 
     //HEADER
     $('[data-id="header"]').css('height', oneSixth);
-
+    
     //INTRO
+    $('[data-id="intro"]').height(oneTenth * 2);
     $('hr').width(totalWidth - 30);
-
-    //FORM
-    $('[data-id="form-login"]').height(oneNineth);
-    $('[data-id="form-login"] input').height(oneNineth / 2 - 11.2);
-
-    //INTRO
-    $('[data-id="intro"]').height(oneSixth * 2);
-    video.css('marginTop', (oneSixth * 2) - video.height() + 'px');
-    $('[data-id="form-submit"]').height(oneSixth - 10);
-    var remain = totalHeight - $('[data-id="header"]').outerHeight(true) - $('hr').outerHeight(true) - $('[data-id="form-login"]').outerHeight(true) - $('[data-id="intro"]').outerHeight(true) - $('[data-id="social-btn"]').outerHeight(true);
-//    alert(remain);
-    if (remain > $('[data-id="form-submit"]').outerHeight(true)) {
-        $('[data-id="form-submit"]').height(remain);
-    }
-
+    
+    //FORM SUBMIT
+//    $('[data-id="form-submit"]').css('height', oneSixth);
+    $('[data-id="form-submit"]').height(oneSixth);
+    
+    $('[data-id="form-login"]').height(oneSixth * 2);
 }
 
 $(document).ready(function () {
     updateSize();
     $('[data-id="intro"] video').autoplay = "true";
     $('[data-id="intro"] video').load();
-
+    
     var gBtn = $('[data-id="google-btn"]');
     var fbBtn = $('[data-id="fb-btn"]');
-
+    
     if (gBtn.width() > fbBtn.width()) {
         fbBtn.width(gBtn.width());
     } else {
