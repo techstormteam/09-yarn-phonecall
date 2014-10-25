6ea37bcd1cd53fe62728788f3b10adb9ef9383ee
Viet Anh <vietanh.sgu@gmail.com>
2014-10-26 01:25:32 +0700

    ringing

--- platforms/android/assets/www/js/ringing.js
+++ platforms/android/assets/www/js/ringing.js
@@ -8,12 +8,15 @@ var footer = $('[data-id="footer"]');
 function updateSize() {
     totalHeight = $(window).height();
     totalWidth = $(window).width();
-    
+
     header.height(totalHeight / 6);
     main.height(totalHeight / 6 * 4);
-    footer.height(totalHeight / 6);
+    footer.height(totalHeight / 8);
     footer.children('div').height(totalHeight / 8);
     footer.children('div').width(totalWidth / 2);
+    var remain = totalHeight - header.outerHeight(true) - main.outerHeight(true) - footer.outerHeight(true); 
+    
+    footer.css('marginTop', remain);
 }
 
 $(document).ready(function () {
