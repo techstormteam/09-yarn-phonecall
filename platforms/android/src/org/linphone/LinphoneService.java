6a5b3d17e970d11e8f6a48a72c0cbf4982a07cfe
vinh.thien0301 <vinh.thien0301@gmail.com>
2014-11-08 10:56:39 +0700

    fix

--- platforms/android/src/org/linphone/LinphoneService.java
+++ platforms/android/src/org/linphone/LinphoneService.java
@@ -43,6 +43,7 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.app.admin.DevicePolicyManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
