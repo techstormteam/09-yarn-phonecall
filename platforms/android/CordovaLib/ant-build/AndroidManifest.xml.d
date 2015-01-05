65856b3c841ab57bf2d69b6e3781ed394081033e
vinh.thien0301 <vinh.thien0301@gmail.com>
2015-01-05 23:43:24 +0700

    fix

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/macbookair/09-yarn-phonecall/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/macbookair/09-yarn-phonecall/platforms/android/CordovaLib/AndroidManifest.xml \
