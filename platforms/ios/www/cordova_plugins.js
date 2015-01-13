e22dfc99f5ecbdd9f9111378e897c9632304695e
vinh.thien0301 <vinh.thien0301@gmail.com>
2015-01-13 08:10:14 +0700

    fix contact in ios

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,41 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
-        "id": "org.apache.cordova.inappbrowser.inappbrowser",
-        "clobbers": [
-            "window.open"
-        ]
-    },
-    {
-        "file": "plugins/com.wezka.nativecamera/www/CameraConstants.js",
-        "id": "com.wezka.nativecamera.Camera",
-        "clobbers": [
-            "Camera"
-        ]
-    },
-    {
-        "file": "plugins/com.wezka.nativecamera/www/CameraPopoverOptions.js",
-        "id": "com.wezka.nativecamera.CameraPopoverOptions",
-        "clobbers": [
-            "CameraPopoverOptions"
-        ]
-    },
-    {
-        "file": "plugins/com.wezka.nativecamera/www/Camera.js",
-        "id": "com.wezka.nativecamera.camera",
-        "clobbers": [
-            "navigator.camera"
-        ]
-    },
-    {
-        "file": "plugins/com.wezka.nativecamera/www/CameraPopoverHandle.js",
-        "id": "com.wezka.nativecamera.CameraPopoverHandle",
-        "clobbers": [
-            "CameraPopoverHandle"
-        ]
-    },
-    {
         "file": "plugins/com.paypal.cordova.mobilesdk/www/cdv-plugin-paypal-mobile-sdk.js",
         "id": "com.paypal.cordova.mobilesdk.PayPalMobile",
         "clobbers": [
@@ -43,28 +8,96 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/com.phonegap.plugins.facebookconnect/facebookConnectPlugin.js",
-        "id": "com.phonegap.plugins.facebookconnect.FacebookConnectPlugin",
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
         "clobbers": [
-            "facebookConnectPlugin"
+            "window.open"
         ]
     },
     {
-        "file": "plugins/com.cabot.plugins.googleplus/www/plugins/googleConnectPlugin.js",
-        "id": "com.cabot.plugins.googleplus.GoogleConnectPlugin",
+        "file": "plugins/org.apache.cordova.contacts/www/contacts.js",
+        "id": "org.apache.cordova.contacts.contacts",
         "clobbers": [
-            "navigator.googleConnectPlugin"
+            "navigator.contacts"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/Contact.js",
+        "id": "org.apache.cordova.contacts.Contact",
+        "clobbers": [
+            "Contact"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactAddress.js",
+        "id": "org.apache.cordova.contacts.ContactAddress",
+        "clobbers": [
+            "ContactAddress"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactError.js",
+        "id": "org.apache.cordova.contacts.ContactError",
+        "clobbers": [
+            "ContactError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactField.js",
+        "id": "org.apache.cordova.contacts.ContactField",
+        "clobbers": [
+            "ContactField"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactFindOptions.js",
+        "id": "org.apache.cordova.contacts.ContactFindOptions",
+        "clobbers": [
+            "ContactFindOptions"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactName.js",
+        "id": "org.apache.cordova.contacts.ContactName",
+        "clobbers": [
+            "ContactName"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactOrganization.js",
+        "id": "org.apache.cordova.contacts.ContactOrganization",
+        "clobbers": [
+            "ContactOrganization"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactFieldType.js",
+        "id": "org.apache.cordova.contacts.ContactFieldType",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ios/contacts.js",
+        "id": "org.apache.cordova.contacts.contacts-ios",
+        "merges": [
+            "navigator.contacts"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ios/Contact.js",
+        "id": "org.apache.cordova.contacts.Contact-iOS",
+        "merges": [
+            "Contact"
         ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.inappbrowser": "0.5.4-dev",
-    "com.wezka.nativecamera": "0.1.2",
     "com.paypal.cordova.mobilesdk": "2.2.1",
-    "com.phonegap.plugins.facebookconnect": "0.11.0",
-    "com.cabot.plugins.googleplus": "0.0.1"
+    "org.apache.cordova.inappbrowser": "0.5.4-dev",
+    "org.apache.cordova.contacts": "0.2.15"
 }
 // BOTTOM OF METADATA
 });
