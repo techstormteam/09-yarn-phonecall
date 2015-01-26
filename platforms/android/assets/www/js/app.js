b2c6bac169f5bc0431ca14f356061f895281ae6d
vinh.thien0301 <vinh.thien0301@gmail.com>
2015-01-26 13:57:01 +0700

    fix contact

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -812,17 +812,17 @@ function doCallLogs() {
 }
 
 function doPhoneContacts() {
-	//  navigator.contacts.pickContact(function(contact){
-	//  alert(JSON.stringify(contact));
-	//  if (contact.phoneNumbers !== null && contact.phoneNumbers.length > 0) {
-	//       setDialedNumber(contact.phoneNumbers[0].value);
-	//  }
-	//},function(err){
-	//console.log('Error: ' + err);
-	//});
-	window.phoneContacts("", function (message) {
-	//empty
+	navigator.contacts.pickContact(function(contact){
+//	  alert(JSON.stringify(contact));
+	  if (contact.phoneNumbers !== null && contact.phoneNumbers.length > 0) {
+	       setDialedNumber(contact.phoneNumbers[0].value);
+	  }
+	},function(err){
+	console.log('Error: ' + err);
 	});
+//	window.phoneContacts("", function (message) {
+//	//empty
+//	});
 }
 
 function openlink(url) {
